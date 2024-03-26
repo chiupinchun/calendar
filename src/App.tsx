73d5854c4bfbf3434d2c44d81585b40fe6dde5c8
Hato <fansintuesan@gmail.com>
2024-03-26 21:49:53 +0800

    feat: basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,33 @@
+import { useMemo, useState } from 'react'
 import './App.css'
+import { getDays } from './utils/getDays'
+
+// Default: white
+// Hover: #e6e6e6
+// Today: #ffff76
+// Active: #006edc
+// Non-Current Month: #757575
 
 function App() {
+  const [year, setYear] = useState(new Date().getFullYear())
+  const [month, setMonth] = useState(new Date().getMonth() + 1)
+  const [date, setDate] = useState(new Date().getDate())
+  const days = useMemo(() => getDays(year, month), [year, month])
 
   return (
     <>
-      <div>123</div>
+      <div className='calendar-header'>
+        <span className='month-select content-center'>&lt;</span>
+        <span>{year}年{month}月</span>
+        <span className='month-select content-center'>&gt;</span>
+      </div>
+      <div className='day-area'>
+        {days.map((day, index) => (
+          <div key={index} className='day-button content-center'>
+            {day}日
+          </div>
+        ))}
+      </div>
     </>
   )
 }
