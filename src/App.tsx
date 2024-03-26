b14aa682d8fb4e0f02f76d61465ff46506867999
Hato <fansintuesan@gmail.com>
2024-03-26 22:38:25 +0800

    feat: calendar basic features

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useMemo, useRef, useState } from 'react'
+import { useCallback, useEffect, useMemo, useRef, useState } from 'react'
 import './App.css'
 import { isSameDay, getDays } from './utils/getDays'
 
@@ -15,6 +15,26 @@ function App() {
   const [date, setDate] = useState(toDay.current.getDate())
   const days = useMemo(() => getDays(year, month), [year, month])
 
+  const notCurrentMonth = useCallback((day: Date) => day.getMonth() + 1 !== month, [month])
+
+  const [selected, setSelected] = useState<Date[]>([])
+  const select = (day: Date) => {
+    if (!selected.length || selected.length >= 2) {
+      setSelected([day])
+    } else {
+      if (day > selected[0]) {
+        const start = days.indexOf(selected[0])
+        const end = days.indexOf(day) + 1
+        setSelected(days.slice(start, end))
+      } else {
+        setSelected([day])
+      }
+    }
+  }
+  useEffect(() => {
+    setSelected([])
+  }, [year, month])
+
   return (
     <>
       <div className='calendar-header'>
@@ -24,10 +44,16 @@ function App() {
       </div>
       <div className='day-area'>
         {days.map((day, index) => (
-          <div key={index} className={[
-            'day-button content-center',
-            isSameDay(day, toDay.current) ? 'today' : ''
-          ].join(' ')} >
+          <div
+            key={index}
+            className={[
+              'day-button content-center',
+              isSameDay(day, toDay.current) ? 'today' : '',
+              notCurrentMonth(day) ? 'none-current-month' : '',
+              selected.includes(day) ? 'active' : ''
+            ].join(' ')}
+            onClick={() => notCurrentMonth(day) || select(day)}
+          >
             {day.getDate()}日
           </div>
         ))}
